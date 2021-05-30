fe07a16351d2483db4adcd30c8dcedca6235398a
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-05-30 23:36:15 +0700

    Slide index

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,3 +1,45 @@
+function CarouselInit() {
+  var checkWidth = $(window).width();
+  var carousel = $(".js-carousel-catmain");
+  if (checkWidth < 1000) {
+    carousel.addClass("owl-carousel").owlCarousel({
+      margin: 0,
+      loop: true,
+      items: 1,
+      nav: false,
+      navSpeed: 800,
+      autoheight: true,
+      autoplay: true,
+      autoplayTimeout: 4000,
+      autoplayHoverPause: true,
+      smartSpeed: 800,
+      responsive: {
+        0: {
+          items: 1,
+          dots: true,
+        },
+        600: {
+          items: 1,
+        },
+      },
+    });
+
+    var owlTwo = $(".js-carousel-catmain");
+    owlTwo.owlCarousel();
+    $(".owl-next-two").click(function () {
+      owlTwo.trigger("next.owl.carousel", [700]);
+    });
+    $(".owl-prev-two").click(function () {
+      owlTwo.trigger("prev.owl.carousel", [700]);
+    });
+  } else {
+    if (carousel.data("owlCarousel") != "undefined") {
+      carousel.trigger("destroy.owl.carousel").removeClass("owl-carousel");
+    }
+  }
+}
+$(document).ready(CarouselInit);
+$(window).resize(CarouselInit);
 
 /* Jquery*/
 $(document).ready(function () {
