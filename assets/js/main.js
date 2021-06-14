78a7d4e32404d97c1a56056ddce1eea4701fab7f
ThienVu97ht <thien.vu97ht123@gmail.com>
2021-06-14 08:56:53 +0700

    NotFound OderSuccess

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,18 @@ function redirectAccount() {
   console.log("xxx");
   window.location.href = "account.html";
 }
+
+var string1 = "Học Javascript tại F8";
+var string2 = "Javascrip";
+
+console.log(string1.indexOf(string2));
+
+function stringInString(needle, haystack) {
+  var result =
+    haystack.indexOf(needle) != -1 &&
+    typeof needle == "string" &&
+    typeof haystack == "string";
+  console.log(result);
+}
+
+stringInString(string2, string1);
